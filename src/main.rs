812af70e85ab2fb83a9386636756a5b26ed4a176
Riley Mathews <rileymathews80@gmail.com>
2026-03-29 21:17:37 -0500

    initial boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{response::Html, routing::get, Router};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+use tracing_subscriber::fmt::format::FmtSpan;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate;
+
+async fn index() -> Html<String> {
+    let template = IndexTemplate;
+    Html(template.render().unwrap())
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .json()
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
